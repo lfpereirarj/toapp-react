72de126488d95c79af629efe80780677c82ab913
Felipe Pereira <lf.pereirarj@gmail.com>
2018-07-28 18:05:24 -0300

    Finalizando projeto

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role='form' className='todo-form'>
-        <div className="col-xs-12 col-sm-9 col-md-10">
-            <input id='description' className='form-control' type="text" placeholder='Adicione uma tarefa'></input>
+export default props => {
+    const keyHandler = (e) => {
+        if(e.key == 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key == 'Escape'){
+            props.handleClear()
+        }
+    }
+    return (
+        <div role='form' className='todo-form'>
+            <Grid cols='12 9 10'>
+                <input id='description' className='form-control' type="text" placeholder='Adicione uma tarefa' onKeyUp={keyHandler} onChange={props.handleChange} value={props.description}></input>
+            </Grid>
+            <Grid cols='12 3 2'>
+                <IconButton style='primary' icon='plus' onClick={props.handleAdd}/>
+                <IconButton style='info' icon='search' onClick={props.handleSearch}/>
+                <IconButton style='default' icon='close' onClick={props.handleClear}/>
+            </Grid>
         </div>
-        <div className="col-xs-12 col-sm-3 col-md-2">
-            <button className="btn btn-primary" type="button">
-                <i className="fa fa-plus"></i>
-            </button>
-        </div>
-    </div>
-)
+    )
+}
